c29f2c260a2cfa50997b4668ac9f0245e0134dc3
Alice Goward <alice.goward@spgs.org>
2023-10-22 23:55:20 +0100

    basic todo list screen working but no way to add tasks

--- app/src/main/java/com/example/a202310212/MainActivity.kt
+++ app/src/main/java/com/example/a202310212/MainActivity.kt
@@ -3,11 +3,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.material3.Checkbox
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
@@ -35,54 +38,57 @@ class MainActivity: ComponentActivity()
         super.onCreate(savedInstanceState)
         prefs = Prefs(applicationContext)
         instance = this
-        addData(prefs!!,"item1",2,false)
-        addData(prefs!!,"item3",2,false)
+        //addData(prefs!!,"item1",2,false)
+        //addData(prefs!!,"item3",2,false)
         setContent {
-            Viewtasks(prefs!!, 2)
+            displayTasks(prefs!!)
             }
         }
     }
 
 @Composable
-fun Viewtasks(prefs: Prefs, day: Int) {
-    var data = readData(prefs)
-    Log.d("another test", data.toString())
-    LazyColumn () {
-        for (i in data.indices) if (data[i].day == day) {
-            item{
-                Row{
-                    var checked by remember {
-                        mutableStateOf (false)
-                    }
-                    var removed by remember {
-                        mutableStateOf (false)
-                    }
-                    Checkbox (
-                        checked = checked,
-                        modifier = Modifier.padding(16.dp),
-                        onCheckedChange = {
-                            checked_ -> checked = checked_
-                            data[i].completed = checked
-                            prefs.mainData = JSON.writeValueAsString(data)
-                            Log.d("CHECKTEST", data[i].toString())
-                        }
-                    )
-                    Text(text = data[i].item,
-                        modifier = Modifier.padding(16.dp), fontSize = 30.sp)
-                    Checkbox(
-                        checked = removed,
-                        modifier = Modifier.padding(16.dp),
-                        onCheckedChange = fun(removed_: Boolean) {
-                            removed = removed_
-                            if (removed) {
-                                data.remove(data[i])
-                                removeData(prefs, i)
-                                // Viewtasks(prefs = prefs, day = day)
-                            }
-                        }
-                    )
+fun TaskItem(
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+) {
+    Row {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        Text(
+            text = taskName,
+            modifier = Modifier.padding(16.dp)
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
 
-                }
+    }
+}
+
+@Composable
+fun displayTasks(prefs: Prefs, modifier: Modifier = Modifier) {
+    var tasks = readData(prefs)
+    Column(modifier = modifier.padding(16.dp)) {
+        for (i in tasks) {
+            var showTask by remember { mutableStateOf(true) }
+            var checked by remember { mutableStateOf(i.completed)}
+            if (showTask) {
+                TaskItem(taskName = i.item, checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                        changeCompletion(prefs,i)
+                        i.completed = it
+                        Log.d(i.completed.toString(),"testithink")
+                    },
+                    onClose = {
+                    showTask = false
+                    removeData(prefs,i.day,i.completed,i.item)
+                    Log.d("CLOSETEST", tasks.toString())
+                })
             }
         }
     }
@@ -92,9 +98,16 @@ fun readData(prefs: Prefs): MutableList<perDayEntry> {
     return JSON.readValue(prefs.mainData!!)
 }
 
-fun removeData(prefs: Prefs, idx: Int) {
+fun changeCompletion(prefs: Prefs,toChange: perDayEntry) {
+    var entries = readData(prefs)
+    entries[entries.indexOf(toChange)].completed = !entries[entries.indexOf(toChange)].completed
+    Log.d("DATADUMP", entries.toString())
+    prefs.mainData = JSON.writeValueAsString(entries)
+}
+
+fun removeData(prefs: Prefs, day: Int, completed: Boolean, item: String) {
     val entries = readData(prefs)
-    entries.remove(entries[idx])
+    entries.remove(perDayEntry(day,item,completed))
     prefs.mainData = JSON.writeValueAsString(entries)
 }
 
