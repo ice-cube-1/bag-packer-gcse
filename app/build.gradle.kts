313fd08cce45ebf5d2a2c8a831191148496e2d9d
Alice Goward <alice.goward@spgs.org>
2023-10-22 18:29:01 +0100

    working complete checkboxes, but remove broken

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -58,7 +58,6 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -67,4 +66,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation ("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.+")
+    implementation("androidx.compose.material:material:1.5.4")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
+
+
 }
--- app/src/main/java/com/example/a202310212/MainActivity.kt
+++ app/src/main/java/com/example/a202310212/MainActivity.kt
@@ -2,14 +2,25 @@ package com.example.a202310212
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.material3.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
-val prefs: Prefs by lazy {
-    MainActivity.prefs!!
-}
 
-data class perDayEntry(var day: Int, var item: String)
+data class perDayEntry(var day: Int, var item: String, var completed: Boolean)
 val JSON = jacksonObjectMapper()
 
 class MainActivity: ComponentActivity()
@@ -24,10 +35,56 @@ class MainActivity: ComponentActivity()
         super.onCreate(savedInstanceState)
         prefs = Prefs(applicationContext)
         instance = this
-        addData(prefs!!,"newitem",2)
-        Log.d("test3",readData(prefs!!).toString())
-        removeData(prefs!!,"hello",1)
-        Log.d("test3",readData(prefs!!).toString())
+        addData(prefs!!,"item1",2,false)
+        addData(prefs!!,"item3",2,false)
+        setContent {
+            Viewtasks(prefs!!, 2)
+            }
+        }
+    }
+
+@Composable
+fun Viewtasks(prefs: Prefs, day: Int) {
+    var data = readData(prefs)
+    Log.d("another test", data.toString())
+    LazyColumn () {
+        for (i in data.indices) if (data[i].day == day) {
+            item{
+                Row{
+                    var checked by remember {
+                        mutableStateOf (false)
+                    }
+                    var removed by remember {
+                        mutableStateOf (false)
+                    }
+                    Checkbox (
+                        checked = checked,
+                        modifier = Modifier.padding(16.dp),
+                        onCheckedChange = {
+                            checked_ -> checked = checked_
+                            data[i].completed = checked
+                            prefs.mainData = JSON.writeValueAsString(data)
+                            Log.d("CHECKTEST", data[i].toString())
+                        }
+                    )
+                    Text(text = data[i].item,
+                        modifier = Modifier.padding(16.dp), fontSize = 30.sp)
+                    Checkbox(
+                        checked = removed,
+                        modifier = Modifier.padding(16.dp),
+                        onCheckedChange = fun(removed_: Boolean) {
+                            removed = removed_
+                            if (removed) {
+                                data.remove(data[i])
+                                removeData(prefs, i)
+                                // Viewtasks(prefs = prefs, day = day)
+                            }
+                        }
+                    )
+
+                }
+            }
+        }
     }
 }
 
@@ -35,15 +92,15 @@ fun readData(prefs: Prefs): MutableList<perDayEntry> {
     return JSON.readValue(prefs.mainData!!)
 }
 
-fun removeData(prefs: Prefs, item: String, day:Int) {
-    var entries = readData(prefs)
-    entries.remove(perDayEntry(day,item))
+fun removeData(prefs: Prefs, idx: Int) {
+    val entries = readData(prefs)
+    entries.remove(entries[idx])
     prefs.mainData = JSON.writeValueAsString(entries)
 }
 
-fun addData(prefs: Prefs, item: String, day: Int) {
-    var entries = readData(prefs)
-    entries.add(perDayEntry(day,item))
+fun addData(prefs: Prefs, item: String, day: Int, completed: Boolean) {
+    val entries = readData(prefs)
+    entries.add(perDayEntry(day,item, completed))
     prefs.mainData = JSON.writeValueAsString(entries)
 
 }
